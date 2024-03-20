052770dd2bd644022df678394fd9d5c8792224bd
Harsha Rai M <86067494+harsharaim@users.noreply.github.com>
2024-03-20 22:52:56 +0530

    bg change

--- src/Components/TextInput.jsx
+++ src/Components/TextInput.jsx
@@ -6,7 +6,7 @@ const TextInput = React.forwardRef(
     ref
   ) => {
     return (
-      <div className="w-full flex flex-col mt-2">
+      <div className="w-full flex flex-col mt-2 ">
         {label && (
           <p className={`text-ascent-2 text-sm mb-2 ${labelStyles}`}></p>
         )}
@@ -19,12 +19,12 @@ const TextInput = React.forwardRef(
             className={`bg-secondary rounded border border-[#66666690] 
             outline-none text-sm text-ascent-1 px-4 py-3 placeholder:text-[#666] ${styles}`}
             {...register}
-            aria-invalid={error?"true":"false"}
+            aria-invalid={error ? "true" : "false"}
           />
         </div>
-        {
-            error && (<span className='text-xs text-[#f64949fe] mt-0.5'>{error}</span>)
-        }
+        {error && (
+          <span className="text-xs text-[#f64949fe] mt-0.5">{error}</span>
+        )}
       </div>
     );
   }
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Logo from "../Components/Assets/logo.png";
 import LoginImg from "../Components/Assets/login.jpg";
+import { TextInput } from "../Components/index.js";
 function Login() {
+  const {
+    register,
+    handlSubmit,
+    formState: { error },
+  } = useForm({
+    mode: onchange,
+  });
   return (
-    <div className="py-16 bg-[#033B0C] w-full h-[100vh]">
+    <div className="py-16 bg-primary w-full h-[100vh]">
       <div className="flex  rounded-xl shadow-lg overflow-hidden m-auto lg:max-w-3xl md:max-w-2xl max-w-sm  ">
         {/* left sisde */}
         <div className="md:w-1/2 md:h-auto bg-[#21890A] w-[90vw] h-[75vh] flex flex-col">
@@ -18,6 +27,20 @@ function Login() {
             </span>{" "}
             ConnectU
           </p>
+          <TextInput
+            name="email"
+            placeholder="example@gmail.com"
+            label="Email Adress"
+            type="email"
+            register={register("email", {
+              required: "Email Address is required",
+            })}
+            // eslint-disable-next-line react/style-prop-object
+            style="w-full rounded-full mx-auto"
+            labelStyle="m1-2"
+            // error={error.email ? error.email.message : ""}
+          />
+         
         </div>
         {/* right side */}
         <div className="hidden md:block w-1/2">
